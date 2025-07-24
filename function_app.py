b3333db6ac12186927448188f8fef4a04047fb82
Muchina-Mungai <muchinahmungai@gmail.com>
2025-07-24 21:26:20 +0300

    Add openai to the source file

--- function_app.py
+++ function_app.py
@@ -2,7 +2,7 @@ import azure.functions as func
 import datetime
 import json
 import logging
-# from openai import AzureOpenAI
+from openai import AzureOpenAI
 # from azure.identity import DefaultAzureCredential
 # from azure.keyvault.secrets import SecretClientpython 
 app = func.FunctionApp()
